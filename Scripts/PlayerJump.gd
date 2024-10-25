16d3e15324a4360273f79cbf038d2f786fca76bc
adudewithapc <axel.holm.mc@hotmail.com>
2024-10-25 21:35:48 +0200

    You can now only jump when on the ground

--- Scripts/PlayerJump.gd
+++ Scripts/PlayerJump.gd
@@ -2,6 +2,8 @@ extends RigidBody2D
 
 @export var jump_height : float = 1
 
+@onready var ground_raycast := $GroundRaycast
+
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("jump"):
+	if Input.is_action_just_pressed("jump") and ground_raycast.is_colliding():
 		apply_impulse(Vector2.UP * jump_height)
