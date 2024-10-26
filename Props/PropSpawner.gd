3904f66b03874f48951cc151ccdcff4dd1fe2184
Chris <78105414+Chriz-code@users.noreply.github.com>
2024-10-26 17:29:36 +0200

    spawn force

--- Props/PropSpawner.gd
+++ Props/PropSpawner.gd
@@ -3,9 +3,9 @@ extends Marker2D
 
 @export var propCollection = []
 
-func spawn_prop(impusle = Vector2.ZERO):
+func spawn_prop(impulse = Vector2.ZERO):
 	var prop: RigidBody2D = propCollection.pick_random().instantiate()
 	prop.global_position = global_position
 	prop.rotation = rotation
 	get_tree().root.add_child(prop)
-	prop.apply_impulse(impusle)
+	prop.apply_impulse(impulse)
