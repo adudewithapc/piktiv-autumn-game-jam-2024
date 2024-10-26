1491e3fa109fe79c6016c104e03489bb179d2098
Nea1303 <62014498+Nea1303@users.noreply.github.com>
2024-10-26 20:30:17 +0200

    Explosion destroys nearby props

--- Scenes/explosion.gd
+++ Scenes/explosion.gd
@@ -1,5 +1,11 @@
 extends Node2D
 
+var deletableProps = ["Block2", "Barrel1", "Block1", "Pumpkin1", "Tree1", "Vase1"]
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	queue_free()
+
+func _on_area_2d_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	print("Collided with " + body.name)
+	if body.name in deletableProps:
+		body.queue_free()
