1e6e4a02bfc642f9bbf7113bb1a6b6c60e48b1fc
adudewithapc <axel.holm.mc@hotmail.com>
2024-10-26 14:06:52 +0200

    Added visual timer until player dies

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -9,6 +9,7 @@ extends RigidBody2D
 @onready var ground_raycast := $GroundRaycast
 @onready var item_spawner := $SpawnPosition
 @onready var stand_still_timer := $StandStillTimer
+@onready var timer_text := $VisualTimer
 
 var horizontal_velocity := 0.0
 
@@ -21,11 +22,7 @@ func _process(delta: float) -> void:
 			jump()
 	
 	update_horizontal_velocity()
-	
-	if is_moving() and not stand_still_timer.is_stopped():
-		stand_still_timer.stop()
-	elif stand_still_timer.is_stopped():
-		stand_still_timer.start()
+	update_timer()
 
 func _physics_process(delta: float) -> void:
 	move_and_collide(Vector2.RIGHT * horizontal_velocity * speed * delta)
@@ -44,7 +41,7 @@ func update_horizontal_velocity():
 	horizontal_velocity = Input.get_axis(input_prefix + "_move_left", input_prefix + "_move_right")
 
 func stand_up(delta: float):
-	if abs(rotation) > 0.5:
+	if abs(rotation) > 1:
 		angular_velocity = -rotation * delta * stand_up_force
 	else:
 		angular_velocity = 0
@@ -52,6 +49,19 @@ func stand_up(delta: float):
 
 func is_moving() -> bool:
 	return abs(horizontal_velocity) + linear_velocity.length() > 0.1
+
+func update_timer():
+	if is_moving():
+		stand_still_timer.stop()
+		timer_text.text = str(stand_still_timer.wait_time).pad_decimals(2)
+	elif stand_still_timer.is_stopped():
+		stand_still_timer.start()
+	
+	if not stand_still_timer.is_stopped():
+		update_timer_text()
+
+func update_timer_text():
+	timer_text.text = str(stand_still_timer.time_left).pad_decimals(2)
 
 func _on_stand_still_timer_timeout() -> void:
 	queue_free()
