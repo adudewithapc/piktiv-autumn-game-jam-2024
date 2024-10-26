8dbccf569a7fb01b7a759fbebabb9dcc4f268a2b
adudewithapc <axel.holm.mc@hotmail.com>
2024-10-26 17:58:37 +0200

    Fixed

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -8,7 +8,6 @@ func _on_play_button_pressed() -> void:
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
-
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
 		get_tree().quit()
