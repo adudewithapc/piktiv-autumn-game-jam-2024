66c1358d79d156076b860201a70a5df3050f6b8f
adudewithapc <axel.holm.mc@hotmail.com>
2024-10-26 20:33:06 +0200

    Credits button

--- Scripts/Menu.gd
+++ Scripts/Menu.gd
@@ -1,9 +1,10 @@
 extends Control
 
-
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/LevelArt.tscn")
 
+func _on_credits_button_pressed() -> void:
+	pass # Replace with function body.
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
