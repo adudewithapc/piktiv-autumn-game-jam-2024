2d913dd2b7b8ab3c929cc76ffc688c167e433ae2
Chris <78105414+Chriz-code@users.noreply.github.com>
2024-10-27 14:48:06 +0100

    renamed

--- Props/Bomb.gd
+++ Props/Bomb.gd
@@ -1,23 +1,24 @@
 extends RigidBody2D
 
 var explosionScene = preload("res://Props/Explosion.tscn")
-var audioScene = preload("res://SFX/SoundPlayer.tscn")
+var audioScene = preload("res://Props/ExplosionSFX.tscn")
 
 func _ready():
 	$ExplodeTimer.timeout.connect(func(): Events.shake_screen.emit(10))
 
 func _on_timer_timeout() -> void:
-	var explosion = explosionScene.instantiate()
-	get_parent().add_child(explosion)
-	explosion.position = position
-	print(get_parent())
-	
-	var audioPlayer = audioScene.instantiate()
-	get_parent().add_child(audioPlayer)
-	audioPlayer.position = position
-	
+	explode()
 	$RemoveTimer.start()
 	
 
 func _on_remove_timer_timeout() -> void:
 	queue_free()
+
+func explode():
+	var explosion = explosionScene.instantiate()
+	get_parent().add_child(explosion)
+	explosion.position = position
+	
+	var audioPlayer = audioScene.instantiate()
+	get_parent().add_child(audioPlayer)
+	audioPlayer.position = position
