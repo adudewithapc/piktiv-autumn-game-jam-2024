0fa103648907f7e7edb0e6c8980b49f0b23c4c1d
Chris <78105414+Chriz-code@users.noreply.github.com>
2024-10-27 14:11:38 +0100

    Update PropSpawner.gd

--- Props/PropSpawner.gd
+++ Props/PropSpawner.gd
@@ -8,5 +8,5 @@ func spawn_prop(impulse = Vector2.ZERO):
 	var prop = chosenPropType.instantiate()
 	prop.global_position = global_position
 	prop.rotation = rotation
-	get_tree().root.get_child(0).add_child(prop)
+	get_tree().root.get_node("/root/Level").add_child(prop)
 	prop.apply_impulse(impulse)
