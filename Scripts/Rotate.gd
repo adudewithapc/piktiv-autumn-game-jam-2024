a58b3451e613298498b4b63ab84100c0b6cd5377
Chris <78105414+Chriz-code@users.noreply.github.com>
2024-10-27 16:00:29 +0100

    rotate particles

--- Scripts/Rotate.gd
+++ Scripts/Rotate.gd
@@ -3,9 +3,10 @@ extends Marker2D
 var rotationDegrees = 0
 @export var rotationIncrease = 0.02
 @export var maxRotationDegrees = 5
+@export var particles: Array[GPUParticles2D] = [] 
 var isRotating = false
 var rotatingRight = true
-   
+
 func _process(delta: float ) -> void:
 	if isRotating:
 		var sign = 1 if rotatingRight else -1
@@ -16,6 +17,7 @@ func _process(delta: float ) -> void:
 func _on_timer_timeout() -> void:
 	isRotating = !isRotating
 	rotationDegrees = 0
+	particles.map(func(o): o.emitting = isRotating)
 	if isRotating:
 		rotatingRight = rotatingRight if randf() > .5 else !rotatingRight
 		
